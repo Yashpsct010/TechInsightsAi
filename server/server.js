557977b3fce7ea37ce788d9ab8ccb1098226405c
Yash Parmar <0808io201056.ies@ipsacademy.org>
2025-03-16 00:08:41 +0530

    cors error resolve

--- server/server.js
+++ server/server.js
@@ -9,7 +9,11 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: process.env.BASE_URL, // Update with your frontend URL
+  methods: "GET,POST,PUT,DELETE",
+  credentials: true, // If using cookies or authentication
+}));
 app.use(express.json());
 
 // Function to connect to MongoDB
